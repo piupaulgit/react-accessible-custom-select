6ed94810c0616ef1146f7cec6c466d18364da715
piupaulgit <contactpiu619@gmail.com>
2023-08-27 11:46:58 +0530

    type

---
+++ storybook-static/assets/Button.stories-ee71e8e6.js
@@ -0,0 +1,4 @@
+import{j as s}from"./jsx-runtime-e6a661ac.js";import"./index-61bf1805.js";import"./_commonjsHelpers-de833af9.js";const t=r=>s("button",{children:r.label});try{t.displayName="Button",t.__docgenInfo={description:"",displayName:"Button",props:{label:{defaultValue:null,description:"",name:"label",required:!0,type:{name:"string"}}}}}catch{}const d={component:t},e={render:()=>s(t,{label:"Button text here"})};var o,a,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <Button label="Button text here" />
+}`,...(n=(a=e.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const u=["Primary"];export{e as Primary,u as __namedExportsOrder,d as default};
+//# sourceMappingURL=Button.stories-ee71e8e6.js.map
---
+++ storybook-static/assets/preview-e330884e.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-3c8527c4.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-776cabad.js"),["./DocsRenderer-3PUGWF3O-776cabad.js","./iframe-3c8527c4.js","./index-61bf1805.js","./_commonjsHelpers-de833af9.js","./react-18-ba2f8a3a.js","./index-6a5bd4ef.js","./index-66ab28d1.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-e330884e.js.map
